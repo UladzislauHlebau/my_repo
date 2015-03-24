7d734c370575a3a20e650c2cf41ec390097eb31d
Uladzislau Hlebau <Uladzislau_Hlebau@epam.com>
2015-03-24 15:07:46 +0300

    Committed the first task using new structure.

---
+++ module 3/task 1/project 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] initialarray = new string [10];
+            Console.WriteLine("Please enter 10 strings:");
+
+            for (int i = 0; initialarray.Length > i; i++)
+            {
+                initialarray [i] = Console.ReadLine();
+            }
+            Console.WriteLine("\nArray with 10 strings:");
+
+            foreach (string val in initialarray)
+            {
+                Console.WriteLine("{0}", val);  
+            }
+
+            string [] reversedStringArray = new string [initialarray.Length];
+            Console.WriteLine("\nReversed list:");
+
+            for (int i = 0; i < initialarray.Length; i++)
+            {
+                reversedStringArray [i] = initialarray [initialarray.Length - i - 1];
+                Console.WriteLine(reversedStringArray [i]);                 
+            }
+            Console.ReadKey();
+        }
+    }
+}
