46596a0c8f36cc6519bfe2859a8088ae46b06318
Uladzislau Hlebau <Uladzislau_Hlebau@epam.com>
2015-05-25 21:17:23 +0300

    Committed corrected 1st task (WebDriver).

---
+++ module 5/task 1/SeleniumWebDriver/LoginToEmail.cs
@@ -0,0 +1,85 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebDriver
+{
+   public class LoginToEmail
+    {
+        private IWebDriver driver;
+
+        public LoginToEmail(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void canLoginEmail()
+        {
+            driver.Navigate().GoToUrl("https://mail.ru/");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            IWebElement element = driver.FindElement(By.CssSelector("input#mailbox__login"));
+            element.SendKeys("vld.gl");
+            IWebElement element1 = driver.FindElement(By.CssSelector("select#mailbox__login__domain"));
+            element1.Click();
+            IWebElement element2 = driver.FindElement(By.XPath("//select[@id='mailbox__login__domain']/option[@value='mail.ru']"));
+            element2.Click();
+            IWebElement element3 = driver.FindElement(By.CssSelector("input#mailbox__password"));
+            element3.SendKeys("WebDriver");
+            IWebElement element4 = driver.FindElement(By.CssSelector("input#mailbox__auth__button"));
+            element4.Click();
+
+        }
+
+        public void canCreateEmail()
+        {
+            IWebElement element5 = driver.FindElement(By.XPath("//span[(text()='Написать письмо')]"));
+            element5.Click();
+
+            IWebElement element6 = driver.FindElement(By.XPath("//textarea[@class='js-input compose__labels__input']"));
+            element6.SendKeys("merrymaker12@gmail.com");
+
+            IWebElement element7 = driver.FindElement(By.XPath("//input[@class='compose__header__field']"));
+            element7.SendKeys("WebDriver Test");
+
+            driver.SwitchTo().Frame(driver.FindElement(By.CssSelector("iframe[title='{#aria.rich_text_area}']")));
+
+            IWebElement element8 = driver.FindElement(By.CssSelector("body#tinymce"));
+            element8.SendKeys("Hey, this is WebDriver Test!");
+
+            driver.SwitchTo().DefaultContent();
+
+            IWebElement element9 = driver.FindElement(By.XPath("//span[(text()='Сохранить')]"));
+            element9.Click();
+
+            IWebElement element10 = driver.FindElement(By.XPath("//span[(text()='Черновики')]"));
+            element10.Click();
+        }
+
+        public void canSendEmail()
+        {
+            IWebElement element11 = driver.FindElement(By.XPath("//div[(text()='merrymaker12@gmail.com')]"));
+            element11.Click();
+
+            IWebElement element12 = driver.FindElement(By.XPath("//span[@class='b-toolbar__btn__text']"));
+            element12.Click();
+
+            IWebElement element13 = driver.FindElement(By.XPath("//span[(text()='Черновики')]"));
+            element13.Click();
+
+        }
+
+        public void findSentEmailInDrafts()
+        {
+            IWebElement element13 = driver.FindElement(By.XPath("//div[(text()='merrymaker12@gmail.com')]"));
+        }
+
+        public string getPageTitle()
+        {
+            return this.driver.Title;
+        }
+    }
+}
---
+++ module 5/task 1/SeleniumWebDriver/MailRuTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumWebDriver
+{
+    [TestFixture]
+    public class MailRuTest
+    {
+        [Test]
+        public void oneCanLoginMailRu()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            LoginToEmail loginToEmail = new LoginToEmail(driver);
+            loginToEmail.canLoginEmail();
+            string title = loginToEmail.getPageTitle();
+            Assert.IsNotNull(title, "Title doesn't exist.");
+            driver.Close();
+        }
+
+        [Test]
+        public void oneCanCreateEmailAndSaveIt()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            LoginToEmail loginToEmail = new LoginToEmail(driver);
+            loginToEmail.canLoginEmail();
+            loginToEmail.canCreateEmail();
+            //this is a check to see if a draft has correct address
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementExists((By.XPath("//div[(text()='merrymaker12@gmail.com')]"))));
+
+            //this is a check to see if a draft has correct subject
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementExists((By.XPath("//div[(text()='WebDriver Test')]"))));
+
+            //this is a check to see if a draft has correct email body
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementExists((By.XPath("//span[(text()='Hey, this is WebDriver Test! С уважением, Vl Gl vld.gl@mail.ru')]"))));
+            driver.Close();
+        }
+
+        [Test]
+        public void oneCanSendEmail()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            LoginToEmail loginToEmail = new LoginToEmail(driver);
+            loginToEmail.canLoginEmail();
+            loginToEmail.canCreateEmail();
+            loginToEmail.canSendEmail();
+
+            Assert.Throws<NoSuchElementException>(() => loginToEmail.findSentEmailInDrafts());
+  
+            driver.Close();
+        }
+
+        //[Test]
+        //public void
+
+
+
+    }
+}
