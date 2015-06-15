2e1e1144a96eb0ffce0d4d36e0a61e41c8d76386
Uladzislau Hlebau <Uladzislau_Hlebau@epam.com>
2015-06-15 16:53:56 +0300

    Module 5, task 3.

---
+++ module 6/task 1/WebDriverPageObject/LoginPage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverPageObject
+{
+    public class LoginPage : Page
+    {
+        private string URL = "https://mail.ru/";
+        private string LOGIN = "vld.gl";
+        private string PASSWORD = "WebDriver";
+
+
+        [FindsBy(How = How.CssSelector, Using = "input#mailbox__login")]
+        public IWebElement loginField;
+
+        [FindsBy(How = How.CssSelector, Using = "input#mailbox__password")]
+        public IWebElement passwordField;
+
+        [FindsBy(How = How.CssSelector, Using = "input#mailbox__auth__button")]
+        public IWebElement loginButton;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this.driver, this);
+        }
+
+        public void openPage()
+        {
+            this.driver.Navigate().GoToUrl(URL);
+        }
+
+        public HomePage loginToEmail()
+        {
+            loginField.SendKeys(LOGIN);
+            passwordField.SendKeys(PASSWORD);
+            loginButton.Click();
+            return new HomePage(this.driver);
+        }
+
+    }
+}
